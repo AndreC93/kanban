34fa6119bc6ac6b05c03ed2ca150ed8078f3d8fe
Andre <fendred@yahoo.com>
2018-12-18 18:20:47 -0500

    Implement Moving Cards Between Columns

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
-import './App.css';
+import './stylesheets/App.css';
 import Column from './components/column.jsx'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.addToCol = this.addToCol.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({
+      columns: this.props.columns,
+    });
+  }
+
+  addToCol(card, colIdx) {
+    this.state.columns[colIdx].cards.push(card);
+    this.setState({
+      columns: this.state.columns,
+    });
+  }
+
   render() {
     return (
-      <div className="App">
+      <div className='App'>
+        <h1>Kanban Board</h1>
+
         { this.props.columns.map( 
-          (col, idx, arr) => 
-          <Column column={col} idx={idx} arr={arr} key={idx} />
-        ) }
+            (col, idx, arr) => 
+            <Column col={col} idx={idx} lastCol={arr.length - 1} key={idx} addToCol={ this.addToCol }/>
+          ) 
+        }
+
       </div>
     );
   }
