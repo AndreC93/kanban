0095628bc49f1091bc6db52e61fd27127fcd6b4e
Andre <fendred@yahoo.com>
2018-12-18 18:20:07 -0500

    Implement AddCard Component

---
+++ src/components/addCard.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import '../stylesheets/card.css'
+
+export default class AddCard extends Component {
+  addCard() {
+    const text = window.prompt("What do you want the card to say?");
+    if(text) this.props.addCard(text);
+  }
+
+  render() {
+    return (
+      <div onClick={ () => this.addCard() } className='card addCard'>
+        <p>+Add a Card</p>
+      </div>
+    )
+  }
+}
