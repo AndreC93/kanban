c54dd33b1f96e6d9807b540916590cf388ddc783
Andre <fendred@yahoo.com>
2018-12-18 18:20:23 -0500

    Implement Arrow Functions

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,10 +1,60 @@
 import React, { Component } from 'react'
+import '../stylesheets/card.css'
+import '../stylesheets/arrow.css'
 
 export default class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.handleArrowClick = this.handleArrowClick.bind(this);
+  }
+
+  handleArrowClick(dir) {
+    if(dir) {
+      const card = this.props.removeCard(this.props.idx);
+      if (!card) return;
+      if (dir === 'Left') {
+        this.props.addToCol(card, this.props.colIdx - 1);
+      } else if (dir === 'Right') {
+        this.props.addToCol(card, this.props.colIdx + 1);
+      }
+    }
+  }
+
+  makeArrow(dir) {
+    if (dir === 'Left' && this.props.leftArrow) {
+        return (
+          <p 
+            className='arrow' 
+            onClick={ () => this.handleArrowClick(dir) }
+          >
+            {"<"}
+          </p>
+        );
+    } else if (dir === 'Right' && this.props.rightArrow) {
+        return (
+          <p
+            className='arrow'
+            onClick={ () => this.handleArrowClick(dir) }
+          >
+            {">"}
+          </p>
+        );
+    }
+    
+    return <p className='arrow'></p>
+  }
+
   render() {
+    const left = this.makeArrow('Left');
+    const right = this.makeArrow('Right');
+
     return (
       <div className='card'>
-        { this.props.card }
+        {left}
+
+        <p>{ this.props.card }</p>
+
+        {right}
       </div>
     )
   }
