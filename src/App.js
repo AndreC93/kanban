3ef307a029128489b3abb3f4af9e2efbbea828ac
Andre <fendred@yahoo.com>
2018-12-19 18:41:56 -0500

    Add Column component implemented in App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
 import './stylesheets/App.css';
-import Column from './components/column.jsx'
+import Column from './components/column.jsx';
+import AddColButton from './components/addColButton.jsx';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {};
     this.addToCol = this.addToCol.bind(this);
+    this.addCol = this.addCol.bind(this);
+    this.defaultColors = ['#2A92BF', '#F4CE46', '#00B961'];
+  }
+
+  addCol() {
+    const col = { cards: [] };
+    col.name = window.prompt('What do you want to name the column?');
+    col.color = this.getColor();
+    this.state.columns.push(col);
+    this.setState({
+      columns: this.state.columns,
+    });
+  }
+
+  getColor() {
+    const color = this.defaultColors.shift();
+    this.defaultColors.push(color);
+    return color;
   }
 
   componentDidMount() {
@@ -33,6 +52,7 @@ class App extends Component {
           ) 
         }
 
+        <AddColButton addCol={ this.addCol }/>
       </div>
     );
   }
